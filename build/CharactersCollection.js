8812182fd220f98ebe007f5238068f6d33fc2e57
Scharif47 <61374955+Scharif47@users.noreply.github.com>
2021-10-31 21:54:56 +0100

    String sort added

---
+++ build/CharactersCollection.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CharactersCollection = void 0;
+var CharactersCollection = /** @class */ (function () {
+    function CharactersCollection(data) {
+        this.data = data;
+    }
+    Object.defineProperty(CharactersCollection.prototype, "length", {
+        get: function () {
+            return this.data.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    CharactersCollection.prototype.compare = function (leftIndex, rightIndex) {
+        return (this.data[leftIndex].toLocaleLowerCase() >
+            this.data[rightIndex].toLocaleLowerCase());
+    };
+    CharactersCollection.prototype.swap = function (leftIndex, rightIndex) {
+        var _a;
+        var characters = this.data.split("");
+        _a = [
+            characters[rightIndex],
+            characters[leftIndex],
+        ], characters[leftIndex] = _a[0], characters[rightIndex] = _a[1];
+        this.data = characters.join("");
+    };
+    return CharactersCollection;
+}());
+exports.CharactersCollection = CharactersCollection;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const charactersCollection = new CharactersCollection("Hello There")
+const numberSorter = new Sorter(numbersCollection);
+const charSorter = new Sorter(charactersCollection);
+numberSorter.sort();
+charSorter.sort();
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
