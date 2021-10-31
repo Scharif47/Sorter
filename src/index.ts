1fbc2173bfea960ed48c3e1b7e52be4bb254a7cb
Scharif47 <61374955+Scharif47@users.noreply.github.com>
2021-10-31 23:10:19 +0100

    LinkedList sort added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./linkedList";
 
-
-const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const charactersCollection = new CharactersCollection("Hello There")
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection("Hello There");
+const linkedList = new LinkedList();
+linkedList.add(200);
+linkedList.add(47);
+linkedList.add(-12);
 const numberSorter = new Sorter(numbersCollection);
 const charSorter = new Sorter(charactersCollection);
+const linkedListSorter = new Sorter(linkedList);
 numberSorter.sort();
 charSorter.sort();
+linkedListSorter.sort();
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+linkedList.print();
